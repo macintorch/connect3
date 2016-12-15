753fcd97e2e97d0b0232779302478b899a05732b
Ainor Syahrizal <macintorch@gmail.com>
2016-12-15 12:47:50 +0800

    adjust imageView tag and logic to know who won the game

--- app/src/main/java/ainor/com/my/connect3/MainActivity.java
+++ app/src/main/java/ainor/com/my/connect3/MainActivity.java
@@ -17,14 +17,18 @@ public class MainActivity extends AppCompatActivity {
 
     int [] gameState = {2,2,2,2,2,2,2,2,2};
 
+    int [][] winningPositions = {{0,1,2},{3,4,5},{6,7,8},{0,4,8},{2,4,6}};
+
     public void dropIn (View view) {
         ImageView counter = (ImageView) view;
 
-        System.out.println(counter.getTag().toString());
+
 
         // convert tag into integer
 
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
+
+
 
         if (gameState[tappedCounter] == 2 ) {
 
@@ -40,6 +44,15 @@ public class MainActivity extends AppCompatActivity {
                 activePlayer = 0;
             }
             counter.animate().translationYBy(1000f).rotation(360).setDuration(100);
+
+            for (int[] winningPosition : winningPositions) {
+                if (gameState[winningPosition[0]] == gameState[winningPosition[1]] &&
+                        gameState[winningPosition[1]] == gameState[winningPosition[2]] &&
+                        gameState[winningPosition[0]] != 2) {
+
+                    System.out.println(gameState[winningPosition[0]]);
+                }
+            }
         }
     }
 
