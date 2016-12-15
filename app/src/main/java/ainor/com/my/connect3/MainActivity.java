bd4159c22287dd4ac34b11d1af495d083d5a4562
Ainor Syahrizal <macintorch@gmail.com>
2016-12-15 10:08:21 +0800

    add counter

--- app/src/main/java/ainor/com/my/connect3/MainActivity.java
+++ app/src/main/java/ainor/com/my/connect3/MainActivity.java
@@ -2,8 +2,18 @@ package ainor.com.my.connect3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void dropIn (View view) {
+        ImageView counter = (ImageView) view;
+
+        counter.setTranslationY(-1000f);
+        counter.setImageResource(R.drawable.yellow);
+        counter.animate().translationYBy(1000f).rotation(360).setDuration(100);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
